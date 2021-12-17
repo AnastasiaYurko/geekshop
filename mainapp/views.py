7e731750f29e913090b07dec41885f5e60e54755
Anastasia Yurko <anastasijajurko@mail.ru>
2021-12-17 17:22:49 +0300

    #lesson7

--- mainapp/views.py
+++ mainapp/views.py
@@ -2,8 +2,82 @@ import random
 
 from django.shortcuts import render, get_object_or_404
 
-
 from mainapp.models import Product, ProductCategory
+
+from django.conf import settings
+from django.core.cache import cache
+
+
+def get_links_menu():
+    if settings.LOW_CACHE:
+        key = 'links_menu'
+        links_menu = cache.get(key)
+        if links_menu is None:
+            links_menu = ProductCategory.objects.filter(is_active=True)
+            cache.set(key, links_menu)
+        return links_menu
+    else:
+        return ProductCategory.objects.filter(is_active=True)
+
+
+def get_category(pk):
+    if settings.LOW_CACHE:
+        key = f'category_{pk}'
+        category = cache.get(key)
+        if category is None:
+            category = get_object_or_404(ProductCategory, pk=pk)
+            cache.set(key, category)
+        return category
+    else:
+        return get_object_or_404(ProductCategory, pk=pk)
+
+
+def get_products():
+    if settings.LOW_CACHE:
+        key = 'products'
+        products = cache.get(key)
+        if products is None:
+            products = Product.objects.filter(is_active=True, category__is_active=True).select_related('category')
+            cache.set(key, products)
+        return products
+    else:
+        return Product.objects.filter(is_active=True, category__is_active=True).select_related('category')
+
+
+def get_product(pk):
+    if settings.LOW_CACHE:
+        key = f'product_{pk}'
+        product = cache.get(key)
+        if product is None:
+            product = get_object_or_404(Product, pk=pk)
+            cache.set(key, product)
+        return product
+    else:
+        return get_object_or_404(Product, pk=pk)
+
+
+def get_products_orederd_by_price():
+    if settings.LOW_CACHE:
+        key = 'products_orederd_by_price'
+        products = cache.get(key)
+        if products is None:
+            products = Product.objects.filter(is_active=True, category__is_active=True).order_by('price')
+            cache.set(key, products)
+        return products
+    else:
+        return Product.objects.filter(is_active=True, category__is_active=True).order_by('price')
+
+
+def get_products_in_category_orederd_by_price(pk):
+    if settings.LOW_CACHE:
+        key = f'products_in_category_orederd_by_price_{pk}'
+        products = cache.get(key)
+        if products is None:
+            products = Product.objects.filter(category__pk=pk, is_active=True, category__is_active=True).order_by('price')
+            cache.set(key, products)
+        return products
+    else:
+        return Product.objects.filter(category__pk=pk, is_active=True, category__is_active=True).order_by('price')
 
 
 def index(request):
@@ -22,51 +96,14 @@ def contact(request):
 
 
 def get_hot_product():
-    return random.sample(list(Product.objects.all()), 1)[0]
-    Product.objects.all()[3:5]
+   products = get_products()
+
+   return random.sample(list(products), 1)[0]
 
 
 def get_same_products(hot_product):
-    products_list = Product.objects.all().filter(category=hot_product.category).exclude(pk=hot_product.pk).select_related('category')[:3]
+    products_list = Product.objects.all().filter(category=hot_product.category).exclude(
+        pk=hot_product.pk).select_related('category')[:3]
     return products_list
 
 
-def products(request, pk=None):
-    links_menu = ProductCategory.objects.all()
-    if pk is not None:
-        if pk == 0:
-            products_list = Product.objects.all()
-            category_item = {
-                'name': 'все',
-                'pk': 0
-            }
-        else:
-            category_item = get_object_or_404(ProductCategory, pk=pk)
-            products_list = Product.objects.filter(category__pk=pk)
-        context = {
-            'links_menu': links_menu,
-            "title": 'Продукты',
-            'category': category_item,
-            'products': products_list
-        }
-        return render(request, 'mainapp/products_list.html', context=context)
-
-    hot_product = get_hot_product()
-    same_products = get_same_products(hot_product)
-    context = {
-        'links_menu': links_menu,
-        "title": 'Продукты',
-        'hot_product': hot_product,
-        'same_products': same_products
-    }
-    return render(request, 'mainapp/products.html', context=context)
-
-
-def product(request, pk):
-    links_menu = ProductCategory.objects.all()
-    context = {
-        'product': get_object_or_404(Product, pk=pk),
-        'links_menu': links_menu
-    }
-
-    return render(request, 'mainapp/product.html', context)
