94e3b0d2e7ed8442a2be185d10703917d9d3af59
Anastasia Yurko <anastasijajurko@mail.ru>
2021-12-17 17:31:26 +0300

    #lesson7

--- mainapp/views.py
+++ mainapp/views.py
@@ -106,4 +106,44 @@ def get_same_products(hot_product):
         pk=hot_product.pk).select_related('category')[:3]
     return products_list
 
+def products(request, pk=None):
+    links_menu = ProductCategory.objects.all()
+    if pk is not None:
+        if pk == 0:
+            products_list = Product.objects.all()
+            category_item = {
+                'name': 'все',
+                'pk': 0
+            }
+        else:
+            category_item = get_object_or_404(ProductCategory, pk=pk)
+            products_list = Product.objects.filter(category__pk=pk)
+        context = {
+            'links_menu': links_menu,
+            "title": 'Продукты',
+            'category': category_item,
+            'products': products_list
+        }
+        return render(request, 'mainapp/products_list.html', context=context)
+
+    hot_product = get_hot_product()
+    same_products = get_same_products(hot_product)
+    context = {
+        'links_menu': links_menu,
+        "title": 'Продукты',
+        'hot_product': hot_product,
+        'same_products': same_products
+    }
+    return render(request, 'mainapp/products.html', context=context)
+
+
+def product(request, pk):
+    links_menu = ProductCategory.objects.all()
+    context = {
+        'product': get_object_or_404(Product, pk=pk),
+        'links_menu': links_menu
+    }
+
+    return render(request, 'mainapp/product.html', context)
+
 
